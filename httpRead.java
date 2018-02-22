9559f1d8ef7d0f8b404a5d0fb357ca00941f88d5
breid026 <36700456+breid026@users.noreply.github.com>
2018-02-22 15:24:06 +0100

    Add files via upload

---
+++ httpRead.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+public class httpRead {
+
+    private String command;
+
+    public httpRead(String command)
+    {
+        this.command=command;
+    }
+    public String returnMSG;
+    public static void http() throws IOException {
+        //if(CHECK FOR VALID REQUEST)
+
+        //else
+        //SEND VALID HEADER TO BROWSER CLIENT
+
+    }
+
+    public String response()
+    {
+        return returnMSG;
+
+    }
+}
